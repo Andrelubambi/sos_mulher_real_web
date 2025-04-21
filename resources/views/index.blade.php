d159a5b56eb4dbb65ef5931bde0f6cc314e2fd30
Andre Lubambi <andrelubambi36@gmail.com>
2025-04-21 21:18:38 +0100

    Alterações locais no index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1557,7 +1557,7 @@ href="{{ asset('vendors/images/favicon-16x16.png') }}"
 		<!-- welcome modal end -->
 		<!-- js -->
 		<script src="{{ asset('vendors/scripts/core.js') }}"></script>
-<script src="{{ asset('vendors/scripts/script.min.js') }}"></script>
+<script src="{{ asset('vendors/scripts/script.min.js') }}"></script> 
 <script src="{{ asset('vendors/scripts/process.js') }}"></script>
 <script src="{{ asset('vendors/scripts/layout-settings.js') }}"></script>
 
