c1d406fac4600b4b9ecc244c22f2d4054246094d
jonas portugall <jonascportugal30@gmail.com>
2025-06-02 12:38:02 +0100

    fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,7 +1,3 @@
-import _ from 'lodash';
-window._ = _;
-
-// Laravel Echo
 import Echo from 'laravel-echo';
 import io from 'socket.io-client';
 
@@ -14,7 +10,6 @@ window.Echo = new Echo({
 
 console.log('Echo initialized', window.Echo);
 
-// Exemplo de listener
 let i = 0;
 window.Echo.channel('user-channel')
     .listen('.UserEvent', (data) => {
--- webpack.mix.js
+++
@@ -1,3 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.js('resources/assets/js/laravel-echo-setup.js', 'public/js');
