d3937b82dc9ecb64752cea7b322680bdb6a3acf9
Andrelubambi <andrelubambi36@gmail.com>
2025-09-20 19:44:14 +0100

    chore: config jitsi

--- app/Http/Controllers/VideoCallController.php
+++ app/Http/Controllers/VideoCallController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class VideoCallController extends Controller
 {
-    //
+    /**
+     * Gerar URL de videochamada para dois usuários
+     */
+    public function generateRoomUrl(Request $request, $userId)
+    {
+        $currentUserId = Auth::id();
+         
+        $minId = min($currentUserId, $userId);
+        $maxId = max($currentUserId, $userId);
+        $roomId = "sala-{$minId}-{$maxId}";
+        
+        $jitsiUrl = "http://jitsi.sosmulherreal.com/{$roomId}";
+        
+        return response()->json([
+            'room_id' => $roomId,
+            'room_url' => $jitsiUrl,
+            'participants' => [$currentUserId, $userId]
+        ]);
+    }
 }
