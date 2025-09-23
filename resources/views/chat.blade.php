0e07999204c1f1f4fb788a5ac9fe147c2e0c47f4
Andrelubambi <andrelubambi36@gmail.com>
2025-09-23 11:14:23 +0100

    fix: path index.js

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -24,7 +24,8 @@
     <!-- Scripts -->
     <script src="https://cdn.socket.io/4.7.5/socket.io.min.js"></script>
     <script src="https://unpkg.com/laravel-echo@1.15.3/dist/echo.iife.js"></script>
-    <script type="module" src="{{ asset('js/chat/index.js') }}"></script>
+    @vite('resources/js/chat/index.js')
+
 </head>
 <body>
     <!-- Connection Status -->
